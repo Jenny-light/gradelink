a747369c92737f3e88c650db3f8a004cae18dc10
Jennifer Ebiuwairo Omoregie <jennylightomoregie@gmail.com>
2025-11-12 08:09:49 +0100

    New line at end of file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,16 @@ import TeacherDashboard from './pages/Teacher/TeacherDashboard';
 import StudentDashboard from './pages/Student/StudentDashboard';
 import Header from './components/Header';
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
+    <div className="min-h-screen bg-gray-100">
       <Header />
-      <div className="container mx-auto p-4">
-        <Routes>
-          <Route path="/" element={<h2 className="text-center text-xl font-semibold">Welcome to GradeLink</h2>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/teacher" element={<TeacherDashboard />} />
-          <Route path="/student" element={<StudentDashboard />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/teacher" element={<TeacherDashboard />} />
+        <Route path="/student" element={<StudentDashboard />} />
+      </Routes>
+    </div>
   );
 }
-
-export default App;
